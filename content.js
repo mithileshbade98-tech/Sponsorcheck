23736849c14556ebc659caa2c559da4360b28a09
mithileshbade98-tech <mbade@hawk.iit.edu>
2023-10-30 01:10:29 -0500

    Working State but needs fixing

---
+++ content.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var textContent = document.body.innerText;
+    console.log("Content of the page: ", textContent);
+});
---
+++ popup.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("read").addEventListener("click", function() {
+      chrome.tabs.executeScript({
+        code: "document.body.innerText"
+      }, function(selection) {
+        if (chrome.runtime.lastError) {
+          alert("An error occurred: " + chrome.runtime.lastError.message);
+        } else {
+          var textContent = selection[0];
+          if (textContent.includes("Clearance") || textContent.includes("Sponsorship")) {
+            alert("No sponsorship");
+          } else {
+            alert("Content of the page: " + textContent);
+          }
+        }
+      });
+    });
+  });
+  
